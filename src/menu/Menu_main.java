95b0ac44f94f44f078009780e5a1d8f4c1ea3d3b
daichi <daichi@DESKTOP-5QJ1H1K>
2018-01-20 15:23:44 +0900

    Menu kadai

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view graph = new Menu_view();
+		graph.setBounds(5,5,755,455);
+		graph.setVisible(true);
+		
+	}
+
+}
